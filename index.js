90ddd089ddaf5cd5bbc0935e3a076dd9aa8f392a
윤영미 <35258834+maie421@users.noreply.github.com>
2019-09-18 22:58:47 +0900

    get post 넘기기

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-const PORT=333;
+const PORT=3333;
 
 // respond with "hello world" when a GET request is made to the homepage
 
 function listeningHandler(){
     console.log(`http://172.0.0.1:${PORT} 서버 시작!`)
 }
+app.get('/',homeHandler);
+app.get('/profile',profileHandler);
+
 app.listen(PORT,listeningHandler);
+
+function homeHandler(req,res){
+    console.log("H Start");
+    res.send("H Start");
+}
+function profileHandler(req,res){
+    console.log("P Start");
+    res.send("P Start");
+}
