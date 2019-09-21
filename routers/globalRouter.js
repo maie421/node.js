70a07118a49fd226b40cf3f539cf32fc28a862ec
윤영미 <35258834+maie421@users.noreply.github.com>
2019-09-21 14:14:27 +0900

    pug layout

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,9 +6,9 @@ import {login,logout} from "../controller/userController"
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home,home);
+globalRouter.get(routes.search,search);
 globalRouter.get(routes.login,login);
 globalRouter.get(routes.join,(req,res)=>res.send("join"));
 globalRouter.get(routes.logout,logout);
-globalRouter.get(routes.search,search);
 
 export default globalRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import {
-  user_home,
   user_detail,
   edit_profile,
   change_password
