d6fcc38a380686e5975bfebc9a5741d2588509fa
yeongmi yoon <35258834+maie421@users.noreply.github.com>
2019-09-23 00:30:30 +0900

    Add files via upload

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,7 +1,16 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
-export const home=(req,res)=>res.render("home",{pageTitle:"Root",videos});
+export const home = async (req, res) => {
+  try {	
+    const videos = await Video.find({});
+    console.log(videos);
+    res.render("home", { pageTitle: "Root", videos });
+  } catch (error) {
+    console.log(error);	  
+    res.render("home", { pageTitle: "Root", videos: [] });
+  }	 
+};
 export const search=(req,res)=>{
     const{
         query:{searchwrd:searchingBy}
@@ -13,13 +22,32 @@ export const search=(req,res)=>{
 export const video_home = (req, res) => res.render("VIDEO HOME");
 
 export const video_getUpload = (req, res) => res.render("video/video_upload",{ pageTitle: "Upload" });
-export const video_postUpload = (req, res) => {
-    const {
-      body: { file, title, description }
-    } = req;
-    // To Do : 비디오 저장 및 업로드
-    res.redirect(routes.videos_detail(33333));
-  };
-export const video_detail = (req, res) => res.render("video/video_detail",{ pageTitle: "Video Detail" });
+export const video_postUpload = async (req, res) => {
+  const {	 
+    body: {title, description },
+    file: { path }
+  } = req;
+  const newVideo =await Video.create({
+    fileUrl:path,
+    title,
+    description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videos_detail(newVideo.id));
+};
+export const video_detail =async (req, res) => {
+  const{
+    params:{ id }
+  }=req;
+  try{
+    //console.log(req.params);
+    const video = await Video.findById(id);
+    console.log(video);
+    res.render("video/video_detail",{ pageTitle: "Video Detail",video});
+  }catch(error){
+    console.log(error);
+    res.redirect(routes.home);
+  }
+};
 export const edit_video = (req, res) => res.render("video/edit_video",{ pageTitle: "Edit Video" });
 export const delete_video = (req, res) => res.render("video/delete_video",{ pageTitle: "Delete Video" });
