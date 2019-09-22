01ff3de324dcc240b702de4aa1bb16f5f118c530
윤영미 <35258834+maie421@users.noreply.github.com>
2019-09-23 01:35:48 +0900

    동영상 업로드 오류 수정

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -9,10 +9,12 @@ import {
   delete_video
 } from "../controller/videoController";
 
+import { uploadVideo } from "../middlewares";
+
 const videoRouter = express.Router();
 //videoRouter.get(routes.home, video_home);
 videoRouter.get(routes.upload, video_getUpload);
-videoRouter.post(routes.upload, video_postUpload);
+videoRouter.post(routes.upload, uploadVideo,video_postUpload);
 
 videoRouter.get(routes.videos_detail(), video_detail);
 videoRouter.get(routes.edit_video, edit_video);
